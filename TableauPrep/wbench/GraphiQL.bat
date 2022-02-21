205e12ae5a733b12e64674536463c1ae628df285
SSh <svshabalov@yahoo.com>
2022-02-21 11:35:27 +0200

    TableauPrep

---
+++ TableauPrep/wbench/GraphiQL.bat
@@ -0,0 +1,11 @@
+"https://dub01.online.tableau.com/metadata/graphiql/"
+"https://dub01.online.tableau.com/relationship-service-war/graphql"
+curl "https://dub01.online.tableau.com/relationship-service-war/graphql" ^
+  -X POST -d @GraphiQL.json ^
+  -H "X-Tableau-Auth:vhOKZWaGRKGF-zNE4WTgVg|S2m7rUbYj0DCWfnAwofFknCS0TW4cTsu" ^
+  -v
+
+
+
+
+
---
+++ TableauPrep/wbench/Sign_in.bat
@@ -0,0 +1,5 @@
+rem curl "https://dub01.online.tableau.com/api/3.9/auth/signin" -X POST -d @signin.xml >response.xml
+curl "https://dub01.online.tableau.com/api/3.9/auth/signin" -v -X POST -d @Sign_in.xml 
+rem curl "https://dub01.online.tableau.com" -X POST -d @signin.xml
+
+
