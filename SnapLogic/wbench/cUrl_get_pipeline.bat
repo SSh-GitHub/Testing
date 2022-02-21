f0c2283c2c1a4b28aa10fd6daddfdf468ff20bfa
SSh <svshabalov@yahoo.com>
2022-02-21 11:32:30 +0200

    SnapLogic

---
+++ SnapLogic/wbench/cUrl_get_pipeline.bat
@@ -0,0 +1,4 @@
+D:\Util\Win\curl\I386\curl -i -X GET https://elastic.snaplogic.com/api/1/rest/public/runtime/snaplogictrial?pipe_name=SSh_pipeline_1 ^
+   -H "Host:elastic.snaplogic.com" ^
+   -H "Authorization:Basic c2VyZ2V5LnNoYWJhbG92QG1ldGFpbnRlZ3JhdGlvbi5pbmZvOk1ldGExOTk3b2Mu" ^ 
+  
